aa26d92de3cb3c7f0f27e8458ed02df7c2066a88
PimpinPenguin <tharidu@ualberta.ca>
2018-11-18 04:42:55 -0700

    moar buttons have been added

--- __main__.py
+++ __main__.py
@@ -3,12 +3,15 @@ import pygame
 import os
 
 pygame.init()
+sim_clock = pygame.time.Clock()
+FPS = 15
 
 image_pos = 0, 0
 step = 0
+picture_index = 0
 step_display = "Current Image %d" % step
 images = os.listdir("images")
-print(images)
+playing = False
 
 black = 0, 0, 0
 white = 255, 255, 255
@@ -16,7 +19,8 @@ red = 255, 0, 0
 green = 0, 255, 0, 0
 blue = 0, 0, 255, 0
 
-image = pygame.image.load(f"images\\{images[0]}")
+image = pygame.image.load(f"images\\{images[step]}")
+step_display = ""
 size = width, height = image.get_rect().size
 screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 pygame.font.init()
@@ -24,26 +28,53 @@ font = pygame.font.SysFont("Modern Sans", 30)
 
 button_next_dim = (width / 2 + 30, height / 2, 60, 20)
 button_prev_dim = (width / 2 - 60,  height - 100, 60, 20)
+button_play_dim = (width / 2 + 90, height / 2, 60, 20)
+button_cycle_dim = (width / 2 - 120, height / 2, 60, 20)
 next_button = pygame.Rect(button_prev_dim)
 prev_button = pygame.Rect(button_next_dim)
+play_button = pygame.Rect(button_play_dim)
+cycle_button = pygame.Rect(button_cycle_dim)
 text_box = pygame.Rect(0, 0, 0, 0)
+
+def next_frame(step, images):
+    print(len(images))
+    if step < len(images) - 1:
+        step += 1
+    return image, step_display
+
+def prev_frame(step, images):
+    if step > 0:
+        step += -1
+    return image, step_display
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN :
             mouse_pos = event.pos
             if next_button.collidepoint(mouse_pos):
                 if step < len(images) - 1:
                     step += 1
-                    image = pygame.image.load(f"images\\{images[step]}")
-                step_display = "Current Image %d" % step
+
             elif prev_button.collidepoint(mouse_pos):
                 if step > 0:
                     step += -1
-                    image = pygame.image.load(f"images\\{images[step]}")
-                    step_display = "Current Image %d" % step
+
+            elif play_button.collidepoint(mouse_pos):
+                playing = not playing
+
+            elif cycle_button.collidepoint(mouse_pos):
+                print("cycle pressed")
+
+        elif event.type == pygame.KEYDOWN :
+            if event.key == pygame.K_RIGHT:
+                if step < len(images) - 1:
+                    step += 1
+            elif event.key == pygame.K_LEFT:
+                if step > 0:
+                    step += -1
+
         elif event.type == pygame.VIDEORESIZE:
             screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
             width, height = screen.get_width(), screen.get_height()
@@ -69,11 +100,25 @@ while True:
 
             button_next_dim = (width // 2 + 60, height - gap_width // 1.5, 60, gap_width // 2)
             button_prev_dim = (width // 2 - 120,  height - gap_width // 1.5, 60, gap_width // 2)
+            button_play_dim = (width // 2 - 180,  height - gap_width // 1.5, 60, gap_width // 2)
+            button_cycle_dim = (width // 2 + 120,  height - gap_width // 1.5, 60, gap_width // 2)
             next_button = pygame.Rect(button_next_dim)
             prev_button = pygame.Rect(button_prev_dim)
+            play_button = pygame.Rect(button_play_dim)
+            cycle_button = pygame.Rect(button_cycle_dim)
 
+    image = pygame.image.load(f"images\\{images[step]}")
     text_screen = font.render(step_display, False, black)
     image = pygame.transform.scale(image, image_size)
+
+    if playing:
+        if step < len(images) - 1:
+            step += 1
+        else:
+            playing = False
+
+        image = pygame.image.load(f"images\\{images[step]}")
+    step_display = "Current Image %d" % step
 
     screen.fill(white)
     screen.blit(image, image_pos)
@@ -81,4 +126,7 @@ while True:
     screen.blit(text_screen, text_pos)
     pygame.draw.rect(screen, green, next_button )
     pygame.draw.rect(screen, red, prev_button)
+    pygame.draw.rect(screen, blue, play_button)
+    pygame.draw.rect(screen, (255, 255, 0), cycle_button)
     pygame.display.flip()
+    sim_clock.tick(FPS)
