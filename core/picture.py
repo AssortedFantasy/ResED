1601c4238151c1f64aa43b14ab5dee43fcaaa27f
AssortedFantasy <jehanzeb.mirza@yahoo.com>
2018-11-17 21:11:31 -0700

    Working picture things

--- core/picture.py
+++ core/picture.py
@@ -1,39 +1,39 @@
 from PIL import Image
 import numpy as np
 
-def imagetoarray(filename):
+def image_to_array(filename):
     # https: // stackoverflow.com / questions / 49271913 / convert - numpy - array - to - rgb - image
     picture = Image.open("%s" %filename)
     nparray = np.asarray(picture, dtype=np.float)
     return nparray
 
-def arraytowave(array):
+def array_to_param(array):
     E = array[:,:,0]
     overdensity = array[:,:,1]
     B = array[:,:,2]
     return E, overdensity, B
 
-def scalearray(array):
-    maximum = np.amax(a)
-    minimum = np.amin(a)
+def scale_array(array):
+    maximum = np.amax(array)
+    minimum = np.amin(array)
     if abs(maximum) >= abs(minimum):
         denom = maximum
-    elif abs(maximum) < abs(minimum):
+    else:
         denom = abs(minimum)
+    if denom == 0:
+        denom = 1
     scaledarray = array / denom
     return scaledarray
 
 
-def rgbsorting(scaledarray):
+def array_to_image(array):
+    # Need to pass scaled arrays.
+    height, width = array.shape
+    one = np.ones((height, width))
+    absolutes = np.abs(array)
 
-
-def arraytoimage(processedarray):
-    image = Image.fromarray(processedarray, "RGB")
-    image.show()
-
-a = np.array([[0, -1, 2],
-             [3, -9.2222, 2],
-             [-6, 7, 8]])
-
-b = scalearray(a)
-c = arraytoimage(b)
+    image = np.zeros((height, width, 3))
+    image[:, :, 1] = (one - absolutes) * 255
+    image[:, :, 2] = (2*one - array - absolutes)/2 * 255
+    image[:, :, 0] = (2*one + array - absolutes)/2 * 255
+    return image
