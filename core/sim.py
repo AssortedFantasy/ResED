a10b3b69a6c9d9c88376d8c6b4440c451c57d05e
AssortedFantasy <jehanzeb.mirza@yahoo.com>
2018-11-17 18:17:50 -0700

    Barebones functional Simulator Created! Success, no documentation yet.

--- core/sim.py
+++ core/sim.py
@@ -24,19 +24,29 @@ class Simulator:
         # First two steps must be DEFINED.
         self.step = 2
 
-        z, self.x, self.y = initital_conditions.shape
+        z, self.height, self.width = initital_conditions.shape
 
         if z != 5:
             raise RuntimeError("invalid initial conditions!")
 
-        data = np.zeros((ARRAY_BREAK_LENGTH, 3, self.y, self.x), dtype=np.float16)
+        data = np.zeros((ARRAY_BREAK_LENGTH, 3, self.height, self.width), dtype=np.float16)
+
+        # Initial calculations done manually.
+
         # First two positions defined
         data[:2, 0, :, :] = initital_conditions[:2, :, :]
         # First velocity is zero, Second is deltaX / t
         data[1, 1, :, :] = (data[1, 0, :, :] - data[0, 0, :, :]) / self.timestep
         # Instead of using the wave equation to calculate a for the two points, its forced into position.
         # So its actually going to be defined using v
-        data[1, 2, :, :] = data[1, 1, :, :] / self.timestep
+        data[0, 2, :, :] = data[1, 1, :, :] / self.timestep
+        # We need to calculate the first acceleration MANUALLY.
+        filters.laplace(data[1, 0, :, :], data[1, 2, :, :], self.edge_conditions, self.edge_value)
+        data[1, 2, :, :] = self.simulation_parameters[0, :, :]
+        data[1, 2, :, :] -= data[1, 1, :, :] * self.simulation_parameters[2, :, :]
+        for forcer in self.forcers:
+            data[1, 2, forcer.position_y, forcer.position_x] += forcer(self.timestep * self.step)
+        data[1, 2, :, :] *= self.simulation_parameters[1, :, :]
         self.sim_data.append(data)
 
     def simulate(self, steps=1):
@@ -44,6 +54,7 @@ class Simulator:
             data = self.sim_data[-1]
             index = self.step % ARRAY_BREAK_LENGTH
 
+            # These are not copies, they are views
             prev_position = data[index-1, 0, :, :]
             prev_velocity = data[index-1, 1, :, :]
             prev_acceleration = data[index-1, 2, :, :]
@@ -51,31 +62,28 @@ class Simulator:
             # Handling split arrays
             if self.step % ARRAY_BREAK_LENGTH == 0:
                 # Split magically
-                new_data = np.zeros((ARRAY_BREAK_LENGTH, 3, self.y, self.x), dtype=np.float16)
+                new_data = np.zeros((ARRAY_BREAK_LENGTH, 3, self.height, self.width), dtype=np.float16)
                 self.sim_data.append(new_data)
                 data = self.sim_data[-1]
 
+            # Also not copies, these are views
             curr_position = data[index, 0, :, :]
-            curr_velocity = data[index, 0, :, :]
-            curr_acceleration = data[index, 0, :, :]
+            curr_velocity = data[index, 1, :, :]
+            curr_acceleration = data[index, 2, :, :]
 
             # Forward Euler Method
             curr_position += prev_position + prev_velocity*self.timestep + 1/2*prev_acceleration*self.timestep**2
             curr_velocity += prev_velocity + prev_acceleration*self.timestep
-
             # Compute Current Acceleration
             # Laplace(U) * E
             filters.laplace(curr_position, curr_acceleration, self.edge_conditions, self.edge_value)
             curr_acceleration *= self.simulation_parameters[0, :, :]
-
             # - B * V
             curr_acceleration -= curr_velocity * self.simulation_parameters[2, :, :]
-
             # Forces
-            for
-
+            for forcer in self.forcers:
+                curr_acceleration[forcer.position_y, forcer.position_x] += forcer(self.timestep*self.step)
             # Forces has become acceleration
-            forces *= self.simulation_parameters[1, :, :]
+            curr_acceleration *= self.simulation_parameters[1, :, :]
 
-            # Compute the current position, velocity using the forward Euler method.
-            self.c
+            self.step += 1
